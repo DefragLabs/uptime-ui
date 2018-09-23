15942379a5e174e4d9a9ea35223a3272b72f1e7f
Pravin-Parkhi <pravin@defraglabs.co>
2018-09-23 17:30:03 +0530

    Complete forgot password view UI.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import LoginView from '../components/auth-module/login-view/login-view';
+import ForgotPasswordView from '../components/auth-module/forgot-password-view/forgot-password-view';
 
 export default () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={LoginView} />
+        <Route exact path='/forgot-password' component={ForgotPasswordView} />
       </Switch>
     </BrowserRouter>
   )
