cdf4e21c906f6a914fda1f38e8e057c283277ee6
Pravin-Parkhi <pravin@defraglabs.co>
2019-01-03 11:53:52 +0530

    Remove monitor menu from side nav bar.

--- src/constants/menu-collection.js
+++ src/constants/menu-collection.js
@@ -1,7 +1,6 @@
 export const SIDEBAR_MENUS = [
     {name: "Dashboard", icon: "dashboard", url: "/dashboard"},
     {name: "Uptime", icon: "arrow circle up", url: "/uptime"},
-    {name: "Monitor", icon: "clock", url: "/monitor"},
     {name: "Integration", icon: "bell outline", url: "/integration"}
 ]
 
